9158643c3b1fb2daaac3a0dc1630f23b1e413d42
sampletext32 <sampletext32@bk.ru>
2020-04-19 17:42:09 +0300

    Add project

---
+++ Vilsker_3/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "russian");
+	cout << "Выполнила студентка группы УТН-111 Кришпенц Александра\n";
+	cout << "Программа вычисления количества молока\n";
+
+	int K, k;
+	cout << "Сколько коров? ";
+	cin >> K;
+	cout << "Сколько коз? ";
+	cin >> k;
+
+	if (K < 2 || K > 10)
+	{
+		cout << "Количество коров неправильное\n";
+	}
+	else if (K < 2 || K > 10)
+	{
+		cout << "Количество коз неправильное\n";
+	}
+	else
+	{
+		double result = K * 25 + k * 2.5;
+		cout << "Суммарное количество молока: " << result << "\n";
+	}
+
+	system("pause");
+
+	return 0;
+}
